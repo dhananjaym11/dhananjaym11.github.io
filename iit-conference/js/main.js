55a3b6d134373d43f8aaac76190c2f3f7d884197
dhananjaym11 <dhananjaymane11@gmail.com>
2016-09-17 19:07:07 +0530

    new changes added

--- iit-conference/js/main.js
+++ iit-conference/js/main.js
@@ -6,7 +6,7 @@ jQuery(function($) {'use strict';
 	});
 
 	$('.navbar-collapse ul li a').on('click', function() {  
-		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+		$('html, body').animate({scrollTop: $(this.hash).offset().top - 89}, 1000);
 		return false;
 	});
 
